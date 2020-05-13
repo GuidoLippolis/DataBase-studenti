17bef1738a5ff85b13f3c194b27db4c655102dd4
GuidoLippolis <47869994+GuidoLippolis@users.noreply.github.com>
2020-05-13 17:02:00 +0200

    Update aeroporto.sql

--- aeroporto.sql
+++ aeroporto.sql
@@ -21,6 +21,17 @@ create table if not exists volo (
     /* foreign key (tipoAereo) references aereo(tipoAereo) DOMANDA: perché non funziona il vincolo? */
 );
 
+create table if not exists voliInternazionali (
+	idVolo char(5),
+    giornoSett varchar(10),
+    cittaPart varchar(10) not null,
+    oraPart numeric(2) not null,
+    cittaArr varchar(10) not null,
+    oraArr numeric(2) not null,
+    tipoAereo varchar(10) not null,
+    primary key (idVolo, giornoSett)
+);
+
 create table if not exists aereo (
     tipoAereo varchar(10),
     numPasseggeri numeric(2),
@@ -46,20 +57,23 @@ insert into aeroporto(citta,nazione,numPiste)
 insert into aeroporto(citta,nazione,numPiste)
 	values('Parigi','Francia',60);
     
+insert into aeroporto(citta,nazione,numPiste)
+	values('Marsiglia','Francia',70);
+    
 insert into volo(idVolo,giornoSett,cittaPart,oraPart,cittaArr,oraArr,tipoAereo)
 	values('AB123','Lunedi','Taranto',10,'Bologna',12,'Linea');
     
 insert into volo(idVolo,giornoSett,cittaPart,oraPart,cittaArr,oraArr,tipoAereo)
-	values('AB456','Martedi','Bologna',14,'Berlino',16,'Turbojet');
+	values('AB456','Martedi','Parigi',14,'Parigi',16,'Turbojet');
     
 insert into volo(idVolo,giornoSett,cittaPart,oraPart,cittaArr,oraArr,tipoAereo)
-	values('AB789','Mercoledi','Roma',11,'Bologna',13,'Regionale');
+	values('AB789','Mercoledi','Parigi',11,'Bologna',13,'Regionale');
     
 insert into volo(idVolo,giornoSett,cittaPart,oraPart,cittaArr,oraArr,tipoAereo)
-	values('BC123','Giovedi','Berlino',17,'Bologna',19,'Linea');
+	values('BC123','Giovedi','Parigi',17,'Bologna',19,'Linea');
     
 insert into volo(idVolo,giornoSett,cittaPart,oraPart,cittaArr,oraArr,tipoAereo)
-	values('BC456','Venerdi','Taranto',12,'Bari',14,'Boeing');
+	values('BC456','Venerdi','Parigi',12,'Bari',14,'Boeing');
     
 insert into aereo(tipoAereo,numPasseggeri,qtaMerci)
 	values('Linea',50,350);
@@ -105,3 +119,11 @@ WHERE cittaArr = 'Bologna'
 order by cittaPart*/
 
 /* DOMANDA: come trovo i voli internazionali? */
+
+/* Trovare le città francesi da cui partono più di 3 voli alla settimana diretti in italia
+SELECT DISTINCT nazione
+FROM aeroporto JOIN volo ON (citta = cittaPart)
+WHERE nazione = 'Francia' AND EXISTS ( SELECT count(*) as NumVoliSett
+			               FROM volo
+                                       GROUP BY cittaPart
+                                       HAVING (NumVoliSett > 3) )*/
