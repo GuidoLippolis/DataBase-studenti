344ba637102b2dc5cf978c80649079e981bf674f
GuidoLippolis <47869994+GuidoLippolis@users.noreply.github.com>
2020-05-14 14:33:35 +0200

    Update test_studenti.sql

--- test_studenti.sql
+++ test_studenti.sql
@@ -2,6 +2,13 @@ drop database if exists universita;
 create database universita;
 use universita;
 
+create table if not exists corso_di_laurea (
+    codice_corso_laurea char(4) check(length(codice_corso_laurea) = 4),
+    nome_corso_laurea varchar(15) not null,
+    descrizione varchar(30),
+    primary key (codice_corso_laurea)
+);
+
 create table if not exists studente (
     matricola numeric(6) check(matricola > 0),
     corso_laurea varchar(15) not null,
@@ -10,6 +17,8 @@ create table if not exists studente (
     data_nascita date not null,
     codice_fiscale char(16) unique check(length(codice_fiscale) = 16),
     primary key(matricola)
+    /* foreign key (corso_laurea) references corso_di_laurea(nome_corso_laurea)
+    vincolo non funzionante */
 );
 
 create table if not exists dipartimento (
@@ -25,10 +34,9 @@ create table if not exists docente (
     cognome varchar(15) not null,
     data_nascita date not null,
     codice_fiscale char(16) unique check(length(codice_fiscale) = 16),
-    primary key(matricola),
-    foreign key (dipartimento) references dipartimento(nome)
+    primary key(matricola)
     /* foreign key (dipartimento) references dipartimento(nome)
-       Qui il vincolo NON funziona: FAILED TO OPEN THE REFERENCED TABLE 'DIPARTIMENTO' */
+       Anche qui il vincolo NON funziona: FAILED TO OPEN THE REFERENCED TABLE 'DIPARTIMENTO' */
 );
 
 create table if not exists modulo (
@@ -53,17 +61,10 @@ create table if not exists esame (
     /* Qui i vincoli funzionano: se provo a registrare un esame di una matricola non presente in STUDENTI dà errore */
 );
 
-create table if not exists corso_laurea (
-    codice_corso_laurea char(4) check(length(codice_corso_laurea) = 4),
-    nome varchar(15) not null,
-    descrizione varchar(30),
-    primary key (codice_corso_laurea)
-);
-
 create table if not exists sede (
     codice char(4) check(length(codice) = 4),
-    indirizzo varchar(15),
-    citta varchar(10),
+    indirizzo varchar(20) not null,
+    citta varchar(10) not null,
     primary key (codice)
 );
 
@@ -76,6 +77,25 @@ create table if not exists sede_dipartimento (
     foreign key (codice_sede) references sede(codice)
 );
 
+/* INSERIMENTO CORSI DI LAUREA */
+
+insert into corso_di_laurea(codice_corso_laurea,nome_corso_laurea)
+	values('ABC1','Informatica');
+    
+insert into corso_di_laurea(codice_corso_laurea,nome_corso_laurea)
+	values('ABC2','Matematica');
+    
+insert into corso_di_laurea(codice_corso_laurea,nome_corso_laurea)
+	values('ABC3','Giurisprudenza');
+    
+insert into corso_di_laurea(codice_corso_laurea,nome_corso_laurea)
+	values('ABC4','Fisica');
+    
+insert into corso_di_laurea(codice_corso_laurea,nome_corso_laurea)
+	values('ABC5','Chimica');
+
+/* INSERIMENTO STUDENTI */
+
 insert into studente(matricola,corso_laurea,nome,cognome,data_nascita,codice_fiscale)
 	values(697768,'Informatica','Guido','Lippolis','2000-02-20','LPPGDU00B20L049S');
 
@@ -85,6 +105,43 @@ insert into studente(matricola,corso_laurea,nome,cognome,data_nascita,codice_fis
 insert into studente(matricola,corso_laurea,nome,cognome,data_nascita,codice_fiscale)
 	values('453675','Informatica','Pierpaolo','Mariani','1998-08-5','MRNPPL98M05L049Z');
     
+insert into studente(matricola,corso_laurea,nome,cognome,data_nascita,codice_fiscale)
+	values('427684','Informatica','Andrea','Costantini','1999-12-28','CSTNDR99T28E506P');
+
+insert into studente(matricola,corso_laurea,nome,cognome,data_nascita,codice_fiscale)
+	values('561769','Informatica','Claudio','Minardi','1999-09-26','MNRCLD99P26L049F');
+   
+insert into studente(matricola,corso_laurea,nome,cognome,data_nascita,codice_fiscale)
+	values('187945','Informatica','Luca','Fabbiano','1999-11-22','FBBLCU99S22L049G');
+    
+insert into studente(matricola,corso_laurea,nome,cognome,data_nascita,codice_fiscale)
+	values('423712','Informatica','Francesco','Scarci','2000-02-23','SCRFNC00B23L049S');
+    
+insert into studente(matricola,corso_laurea,nome,cognome,data_nascita,codice_fiscale)
+	values('285634','Informatica','Simone','Marchitelli','1999-06-21','MRCSMN99H21F205F');
+    
+insert into studente(matricola,corso_laurea,nome,cognome,data_nascita,codice_fiscale)
+	values('955877','Informatica','Gianluca','Fornaro','1998-08-20','FRNGLC98M21L049S');
+    
+/* INSERIMENTO DIPARTIMENTI */
+
+insert into dipartimento(codice,nome)
+	values('A123','Informatica'); 
+    
+insert into dipartimento(codice,nome)
+	values('A456','Matematica'); 
+    
+insert into dipartimento(codice,nome)
+	values('A789','Giurisprudenza'); 
+    
+insert into dipartimento(codice,nome)
+	values('B123','Fisica'); 
+    
+insert into dipartimento(codice,nome)
+	values('B456','Chimica'); 
+    
+/* INSERIMENTO DOCENTI */
+   
 insert into docente(matricola,dipartimento,nome,cognome,data_nascita,codice_fiscale)
 	values(573987,'Informatica','Carmelo','Ardito','1975-07-15','RDTCML75L15A662S');
     
@@ -103,6 +160,8 @@ insert into docente(matricola,dipartimento,nome,cognome,data_nascita,codice_fisc
 insert into docente(matricola,dipartimento,nome,cognome,data_nascita,codice_fiscale)
 	values(674123,'Informatica','Fedelucio','Narducci','1979-04-20','NRDFLC79D20A662N');
 
+/* INSERIMENTO MODULI */
+
 insert into modulo(codice,nome,cfu)
 	values('001','Programmazione',12);
 
@@ -120,7 +179,9 @@ insert into modulo(codice,nome,cfu)
     
 insert into modulo(codice,nome,cfu)
 	values('006','Laboratorio di Informatica',6);
-    
+
+/* INSERIMENTO ESAMI */
+
 insert into esame(matricola_studente,codice_modulo,matricola_docente,data_esame,voto)
 	values(697768,'001',573987,'2019-01-23',28);
     
@@ -141,3 +202,40 @@ insert into esame(matricola_studente,codice_modulo,matricola_docente,data_esame,
     
 insert into esame(matricola_studente,codice_modulo,matricola_docente,data_esame,voto)
 	values(123456,'005',397546,'2019-06-05',30);
+    
+/* INSERIMENTO SEDI */
+
+insert into sede(codice,indirizzo,citta)
+	values('123A','Viale del lavoro','Taranto');
+    
+insert into sede(codice,indirizzo,citta)
+	values('456A','Viale Ionio','Taranto');
+
+insert into sede(codice,indirizzo,citta)
+	values('789A','Corso Umberto','Taranto');
+    
+insert into sede(codice,indirizzo,citta)
+	values('123B','Corso Italia','Taranto');
+    
+insert into sede(codice,indirizzo,citta)
+	values('456B','Via Dante','Taranto');
+    
+insert into sede(codice,indirizzo,citta)
+	values('789B','Via Campania','Taranto');
+    
+/* INSERIMENTO SEDI DIPARTIMENTI */
+
+insert into sede_dipartimento(codice_sede,codice_dipartimento)
+	values('123A','A456');
+    
+insert into sede_dipartimento(codice_sede,codice_dipartimento)
+	values('456A','A123');
+    
+insert into sede_dipartimento(codice_sede,codice_dipartimento)
+	values('789A','A789');
+    
+insert into sede_dipartimento(codice_sede,codice_dipartimento)
+	values('123B','B456');
+    
+insert into sede_dipartimento(codice_sede,codice_dipartimento)
+	values('456B','B123');
